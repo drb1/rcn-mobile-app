431e89f34787a9f663fe1945b413689811276eee
deepak bhattarai <dpkraj578@gmail.com>
2024-02-26 22:41:54 +0545

    [RCN-1] project architecture setup, addded i18n ,added react navigation

---
+++ src/components/News.jsx
@@ -0,0 +1,16 @@
+import { useTranslation } from 'react-i18next';
+import {Text, View, Button, useColorScheme} from 'react-native';
+
+export function NewsScreen({navigation}) {
+    const { t } = useTranslation();
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>{t("description")}</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.push('Home')}
+      />
+    </View>
+  );
+}
