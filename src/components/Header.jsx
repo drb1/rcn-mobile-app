17418dc94522b09812734bc2d68871434925ec46
deepak bhattarai <dpkraj578@gmail.com>
2024-03-12 18:54:51 +0545

    [RCN-11] header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,41 @@
-import * as React from "react";
-import { Header, Icon } from "@rneui/base";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Image } from "react-native";
-import { useRoute } from "@react-navigation/native";
+import * as React from 'react';
+import {Header, Icon} from '@rneui/base';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Image} from 'react-native';
+import {useRoute} from '@react-navigation/native';
 const LeftComponent = () => {
- 
-    return (
-        <>
-        <Image source={require('../assests/rcn-logo-red.webp')} style={{height:50,width:50}} alt="logo image"/>
-        </>
-    )
-} 
+  return (
+    <>
+      <Image
+        source={require('../assests/rcn-logo-red.webp')}
+        style={{height: 40, width: 40}}
+        alt="logo image"
+        resizeMode='contain'
+      />
+    </>
+  );
+};
 export default () => {
-  
   return (
     <Header
-    containerStyle={{}}
+    
       backgroundImageStyle={{}}
-     // barStyle="default"
+      // barStyle="default"
       centerComponent={{
-        text: "Returnee Center Nepal",
-        style: { color: "red",fontWeight:'bold' }
+        text: 'Returnee Center Nepal',
+        style: {color: 'red', fontWeight: 'bold'},
       }}
-      centerContainerStyle={{}}
-     // containerStyle={{ width: 350 }}
-      leftComponent={ <LeftComponent />}
+      centerContainerStyle={{alignItems:'center',justifyContent:'center'}}
+      // containerStyle={{ width: 350 }}
+     leftComponent={<LeftComponent />}
       leftContainerStyle={{}}
       linearGradientProps={{}}
       placement="center"
-      rightComponent={{ icon: "home", color: "red" }}
+      rightComponent={{icon: 'home', color: 'red',size:30}}
+     // leftComponent={{icon: 'home', color: 'red'}}
       rightContainerStyle={{}}
       statusBarProps={{}}
       backgroundColor="white"
     />
   );
-}
+};
